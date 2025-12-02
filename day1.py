55cba41d746b09b9f591bc656426dee3e39cad16
Ehsan <enyazdi@gmail.com>
2025-12-02 20:36:32 +0330

    days 1 & 2 refactored

--- day1.py
+++ day1.py
@@ -6,37 +6,37 @@ def parse_input(data: str) -> list[tuple[str, int]]:
     return rotations
 
 
-def puzzle1(input_file: str) -> int:
+def part1(input_file: str) -> int:
     data = file_handle.readfile(input_file).strip()
     rotations = parse_input(data)
 
     pos = 50
     zero_count = 0
-    for d, l in rotations:
-        pos += l * (1 if d == 'R' else -1)
+    for (direction, distance) in rotations:
+        pos += distance if direction == 'R' else -distance
         pos %= 100
         zero_count += pos == 0
 
     return zero_count
 
 
-def puzzle2(input_file: str) -> int:
+def part2(input_file: str) -> int:
     data = file_handle.readfile(input_file).strip()
     rotations = parse_input(data)
 
     pos = 50
     zero_count = 0
-    for d, l in rotations:
+    for (direction, distance) in rotations:
         pre = pos
-        zero_count += l//100
-        l %= 100
-        pos += l * (1 if d == 'R' else -1)
-        zero_count += (pre != 0 and not 0 < pos < 100)
+        zero_count += distance // 100
+        distance %= 100
+        pos += distance if direction == 'R' else -distance
+        zero_count += pre != 0 and not 0 < pos < 100
         pos %= 100
 
     return zero_count
 
 
 if __name__ == '__main__':
-    print('Day #1, part one:', puzzle1('./input/day1.txt'))
-    print('Day #1, part two:', puzzle2('./input/day1.txt'))
+    print('Day #1, part one:', part1('./input/day1.txt'))
+    print('Day #1, part two:', part2('./input/day1.txt'))
--- day2.py
+++ day2.py
@@ -10,12 +10,12 @@ def parse_input(data: str) -> list[tuple[int, int]]:
     return ranges
 
 
-def puzzle1(input_file: str) -> int:
+def part1(input_file: str) -> int:
     data = file_handle.readfile(input_file).strip()
     ranges = parse_input(data)
 
     sum_invalid_ids = 0
-    for start, end in ranges:
+    for (start, end) in ranges:
         for n in range(start, end+1):
             if re.fullmatch(r'(\d+)\1', str(n)):
                 sum_invalid_ids += n
@@ -23,12 +23,12 @@ def puzzle1(input_file: str) -> int:
     return sum_invalid_ids
 
 
-def puzzle2(input_file: str) -> int:
+def part2_slow(input_file: str) -> int:
     data = file_handle.readfile(input_file).strip()
     ranges = parse_input(data)
 
     sum_invalid_ids = 0
-    for start, end in ranges:
+    for (start, end) in ranges:
         for n in range(start, end+1):
             if re.fullmatch(r'(\d+)\1+', str(n)):
                 sum_invalid_ids += n
@@ -36,6 +36,30 @@ def puzzle2(input_file: str) -> int:
     return sum_invalid_ids
 
 
+def part2(input_file: str) -> int:
+    data = file_handle.readfile(input_file).strip()
+    ranges = parse_input(data)
+
+    invalid_ids = set()
+    max_end = max(end for _, end in ranges)
+    mid = (len(str(max_end))+1) // 2
+    half = str(max_end)[:mid]
+    for n in range(1, int(half)+1):
+        invalid_id = str(n)*2
+        while int(invalid_id) <= max_end:
+            invalid_ids.add(int(invalid_id))
+            invalid_id += str(n)
+
+    invalid_ids = sorted(list(invalid_ids))
+    sum_invalid_ids = 0
+    for (start, end) in ranges:
+        left = bisect.bisect_right(invalid_ids, start)
+        right = bisect.bisect_left(invalid_ids, end)
+        sum_invalid_ids += sum(invalid_ids[left:right])
+
+    return sum_invalid_ids
+
+
 if __name__ == '__main__':
-    print('Day #2, part one:', puzzle1('./input/day2.txt'))
-    print('Day #2, part two:', puzzle2('./input/day2.txt'))
+    print('Day #2, part one:', part1('./input/day2.txt'))
+    print('Day #2, part two:', part2('./input/day2.txt'))
