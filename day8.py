a85508e9e1c2668da64aeca5dd89eb68dafa620b
Ehsan <enyazdi@gmail.com>
2025-12-08 22:34:41 +0330

    day 8 refactored

--- day8.py
+++ day8.py
@@ -1,71 +1,61 @@
 import file_handle
 import math
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 
 Point = namedtuple('Point', ['x', 'y', 'z'])
-
-
-def parse_input(data: str) -> list[Point]:
-    points = [Point(*map(int, line.split(','))) for line in data.splitlines()]
-    return points
 
 
 def distance(p1: Point, p2: Point) -> float:
     return math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2)
 
 
-def generate_edges(points: list[Point]) -> list[tuple]:
-    edges = []
-    n = len(points)
-    for u in range(n):
-        for v in range(u + 1, n):
-            edges.append(((u, v), distance(points[u], points[v]),))
-    return sorted(edges, key=lambda e: e[1])
+def parse_input(data: str) -> tuple[list[Point], list[tuple]]:
+    vertices = [Point(*map(int, line.split(',')))
+                for line in data.strip().splitlines()]
+    edges = [(u, v, distance(vertices[u], vertices[v]),)
+             for u in range(len(vertices))
+             for v in range(u + 1, len(vertices))]
+    edges.sort(key=lambda e: e[2])
+
+    return vertices, edges
+
+
+def kruskal(vertices: list[Point], edges: list[tuple]) -> tuple[list[tuple], set[frozenset]]:
+    MST = []
+    cluster = {v: {v} for v in range(len(vertices))}
+
+    for (u, v, dist) in edges:
+        if cluster[u] is not cluster[v]:
+            MST.append((u, v))
+            cluster[u].update(cluster[v])
+            for node in cluster[v]:
+                cluster[node] = cluster[u]
+            if len(cluster[u]) == len(vertices):
+                break
+
+    clusters = set(map(frozenset, cluster.values()))
+    return MST, clusters
 
 
 def part1(input_file: str) -> int:
     data = file_handle.readfile(input_file).strip()
-    points = parse_input(data)
+    vertices, edges = parse_input(data)
 
-    circuits = {i: {i} for i in range(len(points))}
-    edges = generate_edges(points)
+    _, circuits = kruskal(vertices, edges[:1000])
+    three_largest_sizes = sorted(map(len, circuits), reverse=True)[:3]
 
-    for ((u, v), dist) in edges[:1000]:
-        if circuits[u] is not circuits[v]:
-            circuits[u].update(circuits[v])
-            for node in circuits[v]:
-                circuits[node] = circuits[u]
-
-    max_circuits = [set(), set(), set()]
-    for circuit in circuits.values():
-        if circuit not in max_circuits and len(circuit) > len(max_circuits[2]):
-            max_circuits[2] = circuit
-            if len(max_circuits[2]) > len(max_circuits[1]):
-                max_circuits[1], max_circuits[2] = max_circuits[2], max_circuits[1]
-                if len(max_circuits[1]) > len(max_circuits[0]):
-                    max_circuits[0], max_circuits[1] = max_circuits[1], max_circuits[0]
-
-    return math.prod(map(len, max_circuits))
+    return math.prod(three_largest_sizes)
 
 
 def part2(input_file: str) -> int:
     data = file_handle.readfile(input_file).strip()
-    points = parse_input(data)
+    vertices, edges = parse_input(data)
 
-    circuits = {i: {i} for i in range(len(points))}
-    edges = generate_edges(points)
+    MST, _ = kruskal(vertices, edges)
+    (u, v) = MST[-1]
 
-    for ((u, v), dist) in edges:
-        if circuits[u] is not circuits[v]:
-            circuits[u].update(circuits[v])
-            for node in circuits[v]:
-                circuits[node] = circuits[u]
-            if len(circuits[u]) == len(points):
-                last = (u, v)
-                break
-
-    return points[last[0]].x * points[last[1]].x
+    return vertices[u].x * vertices[v].x
 
 
 if __name__ == '__main__':
